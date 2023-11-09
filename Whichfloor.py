ecb3237222a356787c0fa87de86a0bea3dcfd139
jasmine hernandez <jasminehernandez@jasmines-imac.myfiosgateway.com>
2023-11-08 23:00:16 -0500

    fixed error in whichfloor.py

--- Whichfloor.py
+++ Whichfloor.py
@@ -3,7 +3,7 @@ usernum= int(input("On what floor of the building is your office?"))
 
 while usernum > maximum_stories:
    # "statement for the termination condition is NOT true, which keeps the loop going.."
-    print("You entered: " + str(usernum) + " but there are only " + maximum_stories + " floores in this building. Try again..")
+    print("You entered: " + str(usernum) + " but there are only " + str(maximum_stories) + " floors in this building. Try again..")
     
     usernum= int(input("On what floor of the building is your office?"))
 
